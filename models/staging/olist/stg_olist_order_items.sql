0ac16e2519ad930aaec3ae77f1654623c06a72dd
Ayshan Dashdamirova <ayshandashdamirova@Ayshans-MacBook-Pro.local>
2026-02-11 14:48:33 -0800

    added stg_olist_order_items.sql and its data tests

---
+++ models/staging/olist/stg_olist_order_items.sql
@@ -0,0 +1,9 @@
+select
+  order_id,
+  order_item_id,
+  product_id,
+  seller_id,
+  nullif(shipping_limit_date, '')::timestamp_ntz as shipping_limit_ts,
+  nullif(price, '')::numeric(10,2) as price,
+  nullif(freight_value, '')::numeric(10,2) as freight_value
+from {{ source('olist', 'olist_order_items') }}
