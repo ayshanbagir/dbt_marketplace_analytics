836ec948abc8dd6b6aec56ae63d59e7b983f7cc5
Ayshan Dashdamirova <ayshandashdamirova@Ayshans-MacBook-Pro.local>
2026-02-11 20:49:52 -0800

    staging files updated

--- models/staging/olist/stg_olist_order_items.sql
+++ models/staging/olist/stg_olist_order_items.sql
@@ -3,7 +3,7 @@ select
   order_item_id,
   product_id,
   seller_id,
-  nullif(shipping_limit_date, '')::timestamp_ntz as shipping_limit_ts,
+  nullif(shipping_limit_date, '')::timestamp_ntz as shipping_limit_date,
   nullif(price, '')::numeric(10,2) as price,
   nullif(freight_value, '')::numeric(10,2) as freight_value
 from {{ source('olist', 'olist_order_items') }}
--- models/staging/olist/stg_olist_orders.sql
+++ models/staging/olist/stg_olist_orders.sql
@@ -2,9 +2,9 @@ select
   order_id,
   customer_id,
   order_status,
-  nullif(order_purchase_timestamp, '')::timestamp_ntz as order_purchase_ts,
-  nullif(order_approved_at, '')::timestamp_ntz as order_approved_ts,
-  nullif(order_delivered_carrier_date, '')::timestamp_ntz as order_delivered_carrier_ts,
-  nullif(order_delivered_customer_date, '')::timestamp_ntz as order_delivered_customer_ts,
-  nullif(order_estimated_delivery_date, '')::timestamp_ntz as order_estimated_delivery_ts
+  nullif(order_purchase_timestamp, '')::timestamp_ntz as order_purchase_timestamp,
+  nullif(order_approved_at, '')::timestamp_ntz as order_approved_at,
+  nullif(order_delivered_carrier_date, '')::timestamp_ntz as order_delivered_carrier_date,
+  nullif(order_delivered_customer_date, '')::timestamp_ntz as order_delivered_customer_date,
+  nullif(order_estimated_delivery_date, '')::timestamp_ntz as order_estimated_delivery_date
 from {{ source('olist', 'olist_orders') }}
